9a0f928513bae884e645a73404b927dd0a3013f4
Cody <webb.cody@hotmail.com>
2018-03-15 14:22:32 -0400

    Completed Unity Multiplayer Lab

---
+++ UnityProject/Assets/Scripts/Health.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class Health : NetworkBehaviour {
+
+    public bool destroyOnDeath;
+    public const int maxHealth = 100;
+
+    private NetworkStartPosition[] spawnPoints;
+
+    [SyncVar(hook = "OnChangeHealth")]
+    public int currentHealth = maxHealth;
+
+    public RectTransform healthBar;
+
+    private void Start()
+    {
+        if (isLocalPlayer)
+        {
+            spawnPoints = FindObjectsOfType<NetworkStartPosition>();
+        }
+    }
+
+    public void TakeDamage(int amount)
+    {
+        if(!isServer)
+        {
+            return;
+        }
+
+        currentHealth -= amount;
+        if(currentHealth <= 0)
+        {
+            if(destroyOnDeath)
+            {
+                Destroy(gameObject);
+            }
+            else
+            {
+                currentHealth = maxHealth;
+                RpcRespawn();
+            }
+
+        }
+    }
+
+    [ClientRpc]
+    public void RpcRespawn()
+    {
+        Vector3 spawnPoint = Vector3.zero;
+
+        if(spawnPoints != null && spawnPoints.Length > 0)
+        {
+            spawnPoint = spawnPoints[Random.Range(0, spawnPoints.Length)].transform.position;
+        }
+
+        transform.position = spawnPoint;
+    }
+
+    public void OnChangeHealth(int currentHealth)
+    {
+        healthBar.sizeDelta = new Vector2(currentHealth, healthBar.sizeDelta.y);
+    }
+}
--- UnityProject/Assets/Scripts/PlayerController.cs
+++ UnityProject/Assets/Scripts/PlayerController.cs
@@ -1,20 +1,52 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : NetworkBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+    public GameObject bulletPrefab;
+    public Transform bulletSpawn;
+
+    // Use this for initialization
+    void Start () {
+        
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+        if(!isLocalPlayer)
+        {
+            return;
+        }
+
         var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
         var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
 
         transform.Rotate(0,x,0);
         transform.Translate(0,0,z);
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            CmdFire();
+        }
 	}
+
+    public override void OnStartLocalPlayer()
+    {
+        GetComponent<Renderer>().material.color = Color.blue;
+    }
+
+    [Command]
+    void CmdFire()
+    {
+        var bullet = Instantiate(bulletPrefab, bulletSpawn.position, bulletSpawn.rotation);
+
+        bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * 6;
+
+        NetworkServer.Spawn(bullet);
+
+        Destroy(bullet, 2.0f);
+    }
 }
