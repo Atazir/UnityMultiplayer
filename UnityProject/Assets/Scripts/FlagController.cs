931249ba67ad6ecc03a69067c9867591a0fdcb2c
Cody <webb.cody@hotmail.com>
2018-04-29 22:46:31 -0400

    Final Assignment Submission

---
+++ UnityProject/Assets/Scripts/FlagController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlagController : MonoBehaviour {
+
+    [SerializeField] GameObject myFire;
+
+    public bool isGrabbed;
+
+	// Use this for initialization
+	void Start ()
+    {
+        
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+		if(isGrabbed)
+        {
+            myFire.SetActive(false);
+        }
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            isGrabbed = true;
+        }
+    }
+}
---
+++ UnityProject/Assets/Scripts/FlagSpawner.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class FlagSpawner : NetworkBehaviour
+{
+
+    public GameObject flagPrefab;
+
+    public override void OnStartServer()
+    {
+            var spawnPosition = new Vector3(
+                Random.Range(-8.0f, 8.0f),
+                0.5f,
+                Random.Range(-8.0f, 8.0f));
+
+            var spawnRotation = Quaternion.Euler(
+                0.0f,
+                Random.Range(0, 180),
+                0.0f);
+
+            var flag = Instantiate(flagPrefab, spawnPosition, spawnRotation);
+            NetworkServer.Spawn(flag);
+    }
+}
